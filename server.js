20842bd3a16f9a94d409a73d11b012737ed50f5d
Bhattb2 <budbhatt@gmail.com>
2020-04-10 20:32:42 -0700

    created the weather route

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 'use strict';
 
+//  server build
 require('dotenv').config();
 const express = require('express');
 const app = express();
@@ -22,6 +23,7 @@ function dataFunction (request, response) {
   response.status(200).json(airplanes);
 };
 
+// 
 app.get ('/location', locationFunction);
 function locationFunction (request, response) {
   const geoData = require('./data/geo.json');
@@ -33,6 +35,22 @@ response.status(200).send (locationData);
 }
 
 
+//getting the weather forecast for location
+//  path to weather
+app.get('/weather', weatherFunction);
+function weatherFunction (request, response) {
+  // get data from darksky.json
+  const weatherData = require('./data.darksky.json');
+  const weather = request.query.weather;
+  const weatherBuilder = new WeatherConstructor(daily, weatherData);
+  weatherData.daily.data.forEach(obj =>{
+    waetherArray.push(new WeatherConstructor (obj.time, obj.summary));
+  })
+  response.send(waetherArray);
+  console.log(waetherArray);
+}
+
+// constructor function to get information from geo.json file
 function Location (city, geoData) {
   this.search_query = city;
   this.formatted_query = geoData[0].display_name;
@@ -40,6 +58,8 @@ function Location (city, geoData) {
   this.longitude = geoData[0].lon;
 }
 
+
 app.use('*', (request, response) => response.send('Sorry, that route does not exist.'));
 
+//  activate the PORT
 app.listen(PORT,() => console.log(`Listening on port ${PORT}`));
